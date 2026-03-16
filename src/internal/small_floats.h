91434357a18fb627957ad72956d48c746cbd30c6
Samuel McNally <sammcnally587@gmail.com>
2026-03-16 13:20:47 -0500

    small floats typedefs

---
+++ src/internal/small_floats.h
@@ -0,0 +1,11 @@
+//
+// Created by mcsam on 3/16/26.
+//
+#pragma once
+#ifndef THINTA_SMALL_FLOATS_H
+#define THINTA_SMALL_FLOATS_H
+// idk if this will be used, this is just going to be made ahead of time just in case.
+typedef _Float32 f32;
+typedef _Float64 f64;
+
+#endif //THINTA_SMALL_FLOATS_H

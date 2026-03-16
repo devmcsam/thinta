ad8af37d99eab26f16f7fe6f6fd80be43987ff29
Samuel McNally <sammcnally587@gmail.com>
2026-03-16 13:54:22 -0500

    declerations for fixed size big ints up to 384 bits

---
+++ include/integers/fixed.h
@@ -0,0 +1,40 @@
+//
+// Created by mcsam on 3/16/26.
+//
+#pragma once
+#ifndef THINTA_FIXED_H
+#define THINTA_FIXED_H
+
+typedef struct SInt192 {
+    _BitInt(192) bits;
+} SInt192;
+
+typedef struct UInt192 {
+    unsigned _BitInt(192) bits;
+} UInt192;
+
+typedef struct sint256 {
+    _BitInt(256) bits;
+} sint256;
+
+typedef struct uint256 {
+    unsigned _BitInt(256) bits;
+} uint256;
+
+typedef struct sint320 {
+    _BitInt(320) bits;
+} sint320;
+
+typedef struct uint320 {
+    unsigned _BitInt(320) bits;
+} uint320;
+
+typedef struct sint384 {
+    _BitInt(384) bits;
+} sint384;
+
+typedef struct uint384 {
+    unsigned _BitInt(384) bits;
+} uint384;
+
+#endif //THINTA_FIXED_H
